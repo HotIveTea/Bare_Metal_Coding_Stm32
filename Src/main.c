9badfee5fcb1bbef8b66d4d4974219461d7d4869
HotIveTea <nkimthanh47@gmail.com>
2026-04-09 10:51:40 +0700

    Adding ReadPin function

--- Src/main.c
+++ Src/main.c
@@ -1,26 +1,49 @@
 #include <stdint.h>
 #include "stm32f103.h"
 #include "gpio.h"
-// #define RCC_APB2ENR (*((volatile uint32_t *)0x40021018)) // Dia chi cua RCC la 0x40021000 + them offset cua APB2 se la 0x18
-// #define GPIOC_CRH (*((volatile uint32_t *)0x40011004))   // Dia chi cua GPIOC la 0x40011000 + them offset cua CRH se la 0x04
-// #define GPIOC_ODR (*((volatile uint32_t *)0x4001100C))   // Dia chi cua GPIOC la 0x40011000 + them offset cua ODR se la 0x0C
-// #define GPIOA_CRL (*((volatile uint32_t *)0x40010800))
-// #define GPIOA_ODR (*((volatile uint32_t *)0x4001080C))
+#include "os.h"
+#define STACK_SIZE 100
+uint32_t task1_stack[STACK_SIZE];
+uint32_t task2_stack[STACK_SIZE];
 
+TCB_t tcb1;
+TCB_t tcb2;
+TCB_t tcb3;
+extern TCB_t volatile *current_task;
+extern TCB_t volatile *next_task;
 void delay(volatile uint32_t count)
 {
     while (count--)
     {
     }
 }
-int main(void)
+void Task1(void)
 {
-    GPIO_EnableClock(GPIOA);
-    GPIO_SetPin(GPIOA, 3, GPIO_MODE_OUTPUT_PULL_2MHZ);
+    while (1)
+    {
+        GPIO_Toggle(GPIOA, 2);
+        delay(5000000);
+    }
+}
+void Task2(void)
+{
     while (1)
     {
         GPIO_Toggle(GPIOA, 3);
         delay(500000);
     }
+}
+
+int main(void)
+{
+    GPIO_EnableClock(GPIOA);
+    GPIO_SetPin(GPIOA, 4, GPIO_MODE_INPUT_PULLUD);
+    GPIO_SetPin(GPIOA, 3, GPIO_MODE_OUTPUT_PULL_2MHZ);
+    GPIO_SetPin(GPIOA, 2, GPIO_MODE_OUTPUT_PULL_2MHZ);
+    OS_TaskCreate(&tcb1, Task1, task1_stack, STACK_SIZE);
+    OS_TaskCreate(&tcb2, Task2, task2_stack, STACK_SIZE);
+    current_task = &tcb1;
+    next_task = &tcb2;
+    OS_Start();
     return 0;
 }
