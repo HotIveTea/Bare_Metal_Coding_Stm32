29432de8c006aca0ed6fd9b27cf6d27bff72029d
HotIveTea <nkimthanh47@gmail.com>
2026-04-10 20:54:15 +0700

    Update code

--- Src/gpio.c
+++ Src/gpio.c
@@ -5,19 +5,19 @@ void GPIO_EnableClock(GPIO_TypeDef *GPIOx)
     switch ((uint32_t)GPIOx)
     {
     case GPIOA_PERIPH_BASE:
-        RCC->RCC_APB2ENR |= (1 << 2);
+        RCC->APB2ENR |= (1 << 2);
         break;
     case GPIOB_PERIPH_BASE:
-        RCC->RCC_APB2ENR |= (1 << 3);
+        RCC->APB2ENR |= (1 << 3);
         break;
     case GPIOC_PERIPH_BASE:
-        RCC->RCC_APB2ENR |= (1 << 4);
+        RCC->APB2ENR |= (1 << 4);
         break;
     case GPIOD_PERIPH_BASE:
-        RCC->RCC_APB2ENR |= (1 << 5);
+        RCC->APB2ENR |= (1 << 5);
         break;
     case GPIOE_PERIPH_BASE:
-        RCC->RCC_APB2ENR |= (1 << 6);
+        RCC->APB2ENR |= (1 << 6);
         break;
     default:
         break;
@@ -27,31 +27,31 @@ void GPIO_SetPin(GPIO_TypeDef *GPIOx, uint8_t pin, GPIO_MODE_Typedef mode)
 {
     if (pin < 8)
     {
-        GPIOx->GPIOx_CRL &= ~(0xF << (pin * 4));
-        GPIOx->GPIOx_CRL |= (mode << (pin * 4));
+        GPIOx->CRL &= ~(0xF << (pin * 4));
+        GPIOx->CRL |= (mode << (pin * 4));
     }
     else
     {
-        GPIOx->GPIOx_CRH &= ~(0xF << ((pin - 8) * 4));
-        GPIOx->GPIOx_CRH |= (mode << ((pin - 8) * 4));
+        GPIOx->CRH &= ~(0xF << ((pin - 8) * 4));
+        GPIOx->CRH |= (mode << ((pin - 8) * 4));
     }
 }
 void GPIO_WritePin(GPIO_TypeDef *GPIOx, uint8_t pin, uint8_t state)
 {
     if (state)
     {
-        GPIOx->GPIOx_BSRR = (1 << pin);
+        GPIOx->BSRR = (1 << pin);
     }
     else
     {
-        GPIOx->GPIOx_BRR = (1 << pin);
+        GPIOx->BRR = (1 << pin);
     }
 }
 uint8_t GPIO_ReadPin(GPIO_TypeDef *GPIOx, uint8_t pin)
 {
-    return ((GPIOx->GPIOx_IDR >> pin) & 1);
+    return ((GPIOx->IDR >> pin) & 1);
 }
 void GPIO_Toggle(GPIO_TypeDef *GPIOx, uint8_t pin)
 {
-    GPIOx->GPIOx_ODR ^= (1 << pin);
+    GPIOx->ODR ^= (1 << pin);
 }
